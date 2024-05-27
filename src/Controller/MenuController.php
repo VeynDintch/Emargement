bfc4e1720bc7ffcb7e7cac84c6d0137b1c234a69
Kévin Mori <kevin.mickael.mori@gmail.com>
2024-05-27 11:13:01 +0200

    sécurité terminer

---
+++ src/Controller/MenuController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\security\Http\Attribute\IsGranted;
+
+#[IsGranted('IS_AUTHENTICATED_FULLY')]
+class MenuController extends AbstractController
+{   
+    #[Route('/home', name: 'app_menu')]
+    public function index(): Response
+    {
+        if ($this->isGranted('ROLE_PROFESSEUR')){
+            return $this->render('menu/index.html.twig', [
+                'controller_name' => 'Je suis un prof',
+            ]);
+        }
+    
+    if ($this->isGranted('ROLE_STAGIARE')){
+        return $this->render('menu/index.html.twig', [
+            'controller_name' => 'Je suis un Stagiaire',
+        ]);
+        }
+    if ($this->isGranted('ROLE_ADMIN')){
+        return $this->render('menu/index.html.twig', [
+            'controller_name' => 'Je suis un Admin',
+        ]);
+
+        $utilisateur = new Utilisateur();
+        $formUser = $this->createForm(UtilisateurType::class, $utilisateur ,array(action($this->generateUrl('app_utilisateur_new'))));
+        $session = new Session();
+        $formSession = $this->createForm(SessionType::class, $session,arry(action($this->generateUrl('app_utilisateur_new'))));
+        $Promotion = new Promotion();
+        $formPromotion =  $this->createForm(PromotinType::class, $promotin,arry(action($this->generateUrl('app_promotion_new'))));
+        }
+    }
+}
--- src/Security/ConnexionAuthenticator.php
+++ src/Security/ConnexionAuthenticator.php
@@ -50,7 +50,7 @@ class ConnexionAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('utilisateur'));
+    return new RedirectResponse($this->urlGenerator->generate('app_menu'));
         //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
